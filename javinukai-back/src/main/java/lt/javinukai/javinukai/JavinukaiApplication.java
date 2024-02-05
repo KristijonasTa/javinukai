7e95c8477843423a0c381c5efc7a000a09b7af45
KristijonasTa <97470880+KristijonasTa@users.noreply.github.com>
2024-02-05 12:06:08 +0200

    Update JavinukaiApplication.java

--- javinukai-back/src/main/java/lt/javinukai/javinukai/JavinukaiApplication.java
+++ javinukai-back/src/main/java/lt/javinukai/javinukai/JavinukaiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavinukaiApplication {
 
 	public static void main(String[] args) {
+		\\test
 		SpringApplication.run(JavinukaiApplication.class, args);
 	}
 
